e8727ab7a45ff1983c10ae82cd9109ef18b3c592
Sebastian Pinto <sebastianpinto@wposs.com>
2023-01-20 17:03:37 -0500

    Implementation of back button and loading status in track view

--- app/src/main/java/com/example/lastfmapp/ui/track/TrackFragment.kt
+++ app/src/main/java/com/example/lastfmapp/ui/track/TrackFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.lastfmapp.R
@@ -15,6 +16,8 @@ import com.example.lastfmapp.domain.ArtistRepositoryImpl
 import com.example.lastfmapp.domain.RetrofitClient
 import com.example.lastfmapp.presentation.ArtistViewModel
 import com.example.lastfmapp.core.Result
+import com.example.lastfmapp.core.hide
+import com.example.lastfmapp.core.show
 import com.example.lastfmapp.presentation.ArtistViewModelFactory
 import com.example.lastfmapp.ui.track.adapter.TrackAdapter
 
@@ -33,19 +36,19 @@ class TrackFragment : Fragment(R.layout.fragment_track) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentTrackBinding.bind(view)
 
-        binding.collapsingToolbar.title = args.artistName
+        showArtistInfo()
+        goToArtistView()
 
-        //binding.tvArtistName.text = args.artistName
-
-        Glide.with(requireContext()).load(args.urlPicture).centerCrop().into(binding.imgBackground)
 
         args.mbid.let {
             viewModel.getTop5Tracks(it).observe(viewLifecycleOwner) { result ->
                 when(result) {
                     is Result.Loading -> {
+                        binding.containerItemViewLoading.show()
                         Log.d("LiveData", "TrackFragment: Loading..")
                     }
                     is Result.Success -> {
+                        binding.containerItemViewLoading.hide()
                         Log.d("LiveData", "TrackFragment Success: ${result.data}")
                         if (result.data.toptracks.track.isEmpty()) {
                             Toast.makeText(
@@ -58,11 +61,24 @@ class TrackFragment : Fragment(R.layout.fragment_track) {
                         binding.rvTrack.adapter = TrackAdapter(result.data.toptracks.track)
                     }
                     is Result.Failure -> {
+                        binding.containerItemViewLoading.hide()
                         Log.d("LiveData", "TrackFragment Error: ${result.exception}")
                     }
                 }
             }
         }
 
+    }
+
+    private fun showArtistInfo() {
+        Glide.with(requireContext()).load(args.urlPicture).centerCrop().into(binding.imgBackground)
+        binding.collapsingToolbar.title = args.artistName
+
+    }
+
+    private fun goToArtistView() {
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack();
+        }
     }
 }
